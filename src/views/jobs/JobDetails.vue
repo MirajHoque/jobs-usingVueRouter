f2f6fe0d45643c98a763078a53d6e1277443a689
MirajHoque <a.h.miraj2031@gmail.com>
2021-07-13 16:32:19 +0600

    route parameter

---
+++ src/views/jobs/JobDetails.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Job Details Page</h1>
+  <h2>The job id is: {{ id }}</h2>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      id: this.$route.params.id
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
