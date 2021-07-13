2f5b063c854055439f15123bf1f4c5430ef48dd8
MirajHoque <a.h.miraj2031@gmail.com>
2021-07-13 23:22:41 +0600

    fetch data
    fetch using json server and promise

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,17 +1,23 @@
 <template>
-  <h1>Job Details Page</h1>
-  <h2>The job id is: {{ id }}</h2>
+  <h1>{{ job.title }}</h1>
+  <p>The job id is: {{ id }}</p>
+  <p>{{ job.details }}</p>
 </template>
 
 <script>
 export default {
- /* data(){
-    return{
-      id: this.$route.params.id
-    }
-  }
-  */
- props:['id']
+ props:['id'],
+ data() {
+   return {
+     job: null
+   }
+ },
+ mounted() {
+   fetch('http://localhost:3000/jobs' + this.id) //props id
+   .then(res=>res.json())
+   .then(data=> this.job=data)
+   .catch(err=> console.log(err))
+ }
 
 }
 </script>
--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -10,13 +10,16 @@
 export default {
   data(){
     return{
-      jobs: [
-        {title: 'Ninja UX Designer', id: 1, details: 'lorem'},
-        {title: 'Ninja Web Developer', id: 2, details: 'lorem'},
-        {title: 'Ninja Vue Developer', id: 3, details: 'lorem'}
-      ]
+      jobs: []
     }
+  },
+  mounted(){
+    fetch('http://localhost:3000/jobs')
+    .then((res)=>res.json()) //if annonymous function has only parameter doesn't mandatory to use ()
+    .then(data=> this.jobs=data)
+    .catch(err=> console.log(err.message))
   }
+
 
 }
 </script>
@@ -27,7 +30,7 @@ export default {
   padding: 20px;
   border-radius: 10px;
   margin: 10px auto;
-  max-width: 600;
+  max-width: 600px;
   cursor: pointer;
   color: #444
 }
